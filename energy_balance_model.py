680aac8d8b1397d197d1eae37996caa7cce0bc52
dtgoek <goek.deniz@gmail.com>
2022-06-07 16:01:34 +0200

    new comments SEBM functions

--- energy_balance_model.py
+++ energy_balance_model.py
@@ -8,29 +8,43 @@ from scipy import interpolate
 
 
 '''
-Surface energy balance functions
+Functions to caclulate surface energy balance componentents and solve for debris thickness
 
-implemented by Deniz Gök
+Deniz Gök, German Research Centre for Geosciences, Potsdam
 d_goek@gfz-potsdam.de
-'''
 
-#SHORTWAVE RADIATION AND SOLAR GEOMETRY
-'''
-Python implementation of the R Package Insol from Javier G. Corrripio
+
+SHORTWAVE RADIATION AND SOLAR GEOMETRY
+Python implementation of the R Package Insol from Javier G. Corrripio (2003)
 https://doi.org/10.1080/713811744
-https://github.com/cran/insol/blob/master/man/insol-package.Rd
 '''
 
 
 def JD(d):
-    """Convert a datetime object to a julian date.
-    A Julian date is the decimal number of days since January 1, 4713 BCE."""
+    """
+    Convert a datetime object to a julian date.
+
+    Parameters:
+    d (datetime object)
+
+    Returns:
+    JulianDay (float)
+    """
     seconds_per_day = 86400
-    return d.timestamp() / seconds_per_day + 2440587.5
-    
+    JulianDay = d.timestamp() / seconds_per_day + 2440587.5
+
+    return JulianDay
+
 def eqtime(jd):
-    """Calculate the equation of time.
-    See https://en.wikipedia.org/wiki/Equation_of_time.
+    """
+    Calculate the equation of time.
+
+    Parameters:
+    jd (float)
+
+    Returns:
+    EqTime_deg (float)
+
     """
     jdc = (jd - 2451545.0) / 36525.0
     sec = 21.448 - jdc * (46.8150 + jdc * (0.00059 - jdc * 0.001813))
@@ -49,10 +63,23 @@ def eqtime(jd):
         + 4.0 * ecc * y * np.sin(gmas) * np.cos(2 * rl0)\
         - 0.5 * y * y * np.sin(4 * rl0) \
         - 1.25 * ecc * ecc * np.sin(2 * gmas)
-    return np.rad2deg(EqTime) * 4
-	 
+    EqTime_deg = np.rad2deg(EqTime) * 4
+
+    return EqTime_deg
+
 def hourangle(jd, longitude, timezone):
-    """Function for solar position calculation."""
+    """
+    Function for solar position calculation.
+
+    Parameters:
+    jd (float)
+    longitude (float)
+    timezone (int)
+
+    Returns:
+    omega_r (float)
+
+    """
     hour = ((jd-np.floor(jd))*24+12) % 24
     time_offset=eqtime(jd)
     standard_meridian=timezone * 15
@@ -60,8 +87,18 @@ def hourangle(jd, longitude, timezone):
     omega_r = np.pi * (
         ((hour + delta_longitude_time + time_offset / 60) / 12.0) - 1.0)
     return omega_r
-  
+
 def sunr(jd):
+    """
+    Earth-Sun distance in unit AU.
+
+    Parameters:
+    jd (float)
+
+    Returns:
+    R (float)
+
+    """
     # Julian Centuries (Meeus, Astronomical Algorithms 1999. (24.1))
     T = (jd - 2451545)/36525.0
     # mean obliquity of the ecliptic (21.2)
@@ -76,10 +113,20 @@ def sunr(jd):
     v = M + C
     # Sun's Radius Vector (24.5)
     R = (1.000001018*(1-e**2))/(1 + e*np.cos(np.radians(v)))
-    return(R)
+
+    return R
 
 def declination(jd):
-    """Compute the declination of the sun on a given day."""
+    """
+    Compute the declination of the sun on a given day.
+
+    Parameters:
+    jd (float)
+
+    Returns:
+    R (float)
+
+    """
     jdc = (jd - 2451545.0) / 36525.0
     sec = 21.448 - jdc * (46.8150 + jdc * (0.00059 - jdc * .001813))
     e0 = 23.0 + (26.0 + (sec / 60.0)) / 60.0
@@ -94,37 +141,61 @@ def declination(jd):
     suntl = l0 + seqcent
     sal = suntl - 0.00569 - 0.00478 * np.sin(np.deg2rad(125.04 - 1934.136 * jdc))
     delta = np.arcsin(np.sin(np.deg2rad(oblcorr)) * np.sin(np.deg2rad(sal)))
-    return np.rad2deg(delta)
-    
+
+    delta_deg = np.rad2deg(delta)
+    return delta_deg
+
 def sunvector(jd,latitude,longitude,timezone):
-    # https://github.com/cran/insol/blob/master/R/sunvector.R
+    """
+    Calulate unit vector in direction of sun from observer point
+
+    Parameters:
+    jd (float)
+    latitude (float)
+    longitude (float)
+    timezone (int)
+
+    Returns:
+    sv (tuple)
+    """
     omega=hourangle(jd,longitude,timezone)
     delta = np.radians(declination(jd))
     lat_rad = np.radians(latitude)
     svx = -np.sin(omega)*np.cos(delta)
     svy = np.sin(lat_rad)*np.cos(omega)*np.cos(delta)-np.cos(lat_rad)*np.sin(delta)
     svz = np.cos(lat_rad)*np.cos(omega)*np.cos(delta)+np.sin(lat_rad)*np.sin(delta)
-    return(svx,svy,svz)
-    
+    sv = (svx,svy,svz)
+    return sv
+
 def sunpos(sunv):
+    """
+    Azimith and zenith angles from unit vector to the sun from observer postion, Corripio (2003)
+
+    Parameters:
+    sv (tuble)
+
+    Returns:
+    sp (tuple)
+    """
+
     azimuth = np.degrees(np.pi - np.arctan2(sunv[0],sunv[1]))
     zenith = np.degrees(np.arccos(sunv[2]))
-    return (azimuth,zenith)    
+    sp = (azimuth,zenith)
+    return sp
 
 #DEM CALCULATIONS - SHADOW
 
 def gradient(grid, length_x, length_y=None):
     """
-    Computes a unit vector normal to every grid cell in a digital elevation model.
-    
-    Calculate the numerical gradient of a matrix in X, Y and Z directions.
-    :param grid: Matrix or np.array (DEM)
-    :param length_x: Length between two columns
-    :param length_y: Length between two rows
-    :return:
-    
-    https://github.com/tomderuijter/python-dem-shadows/blob/master/python_dem_shadows/gradient.py
+    Computes a unit vector normal to every grid cell in a digital elevation model (DEM).
+
+    Parameters:
+    grid (2darray) #DEM
+
+    Returns:
+    grad (numpy.ndarray) #tensor
     """
+
     if length_y is None:
         length_y = length_x
 
@@ -160,9 +231,13 @@ def check_gradient(grad):
 def hill_shade(grad, sun_vector):
     """
     Compute the intensity of illumination on a surface given the sun position.
-    :param grad:
-    :param sun_vector:
-    :return:
+
+    Parameters:
+    grad (numpy.ndarray) #tensor
+    sun_vector (tuple)
+
+    Returns:
+    hsh (2darray)
     """
     check_gradient(grad)
 
@@ -171,13 +246,11 @@ def hill_shade(grad, sun_vector):
         grad[:, :, 1] * sun_vector[1] +
         grad[:, :, 2] * sun_vector[2]
     )
-    # Remove negative incidence angles - indicators for self-shading.
-    hsh = (hsh + abs(hsh)) / 2.
 
+    hsh = (hsh + abs(hsh)) / 2.
     return hsh
 
-def _cast_shadow(row, col, rows, cols, dl, in_sun, inverse_sun_vector,
-                 normal_sun_vector, z):
+def _cast_shadow(row, col, rows, cols, dl, in_sun, inverse_sun_vector, normal_sun_vector, z):
     n = 0
     z_previous = -sys.float_info.max
 
@@ -201,7 +274,7 @@ def _cast_shadow(row, col, rows, cols, dl, in_sun, inverse_sun_vector,
         else:
             z_previous = z_projection
         n += 1
- 
+
 def _normalize_sun_vector(sun_vector):
     normal_sun_vector = np.zeros(3)
     normal_sun_vector[2] = np.sqrt(sun_vector[0] ** 2 + sun_vector[1] ** 2)
@@ -210,8 +283,8 @@ def _normalize_sun_vector(sun_vector):
     return normal_sun_vector
 
 def _invert_sun_vector(sun_vector):
-    return -sun_vector / max(abs(sun_vector[:2])) 
-                
+    return -sun_vector / max(abs(sun_vector[:2]))
+
 def project_shadows(dem, sun_vector, dx, dy=None):
     """Cast shadows on the DEM from a given sun position."""
 
@@ -254,78 +327,104 @@ def project_shadows(dem, sun_vector, dx, dy=None):
     return in_sun.T
 
 #METEO
-def relative_humidity(TaK, TdK):
-    '''
-    Tak: air temperature  [K]
-    TdK: dew point temperature [K]
+def relative_humidity(Tair_K, Tdew_K):
 
-    https://bmcnoldy.rsmas.miami.edu/Humidity.html
-    Alduchov, O. A., and R. E. Eskridge, 1996: Improved Magnus' form approximation of saturation vapor pressure. J. Appl. Meteor., 35, 601–609.
-    August, E. F., 1828: Ueber die Berechnung der Expansivkraft des Wasserdunstes. Ann. Phys. Chem., 13, 122–137.
-    Magnus, G., 1844: Versuche über die Spannkräfte des Wasserdampfs. Ann. Phys. Chem., 61, 225–247. 
-    '''
-    
+    """
+    Calucates the relativity humidity [%] based on the dewpoint and air temperature [K]
+    https://bmcnoldy.rsmas.miami.edu/Humidity.html, acessed   (Alduchov et al. 1996)
+
+    Parameters:
+    Tair_K (float)
+    Tdew_K (float)
+
+    Returns:
+    rh (float)
+    """
+
+
     c  = 243.04
     b  = 17.625
-    rh = 100 * np.e**( (c * b * (TdK - TaK)) 
-    / ((c + TaK) * (c + TdK)) )
-    
+    rh = 100 * np.e**( (c * b * (Tdew_K - Tair_K))
+    / ((c + Tair_K) * (c + Tdew_K)) )
+
     return rh
 
 def z2p(z):
-    P0=101325
-    T0=288.15
+    """
+    Computes air pressure (hPa) for a given altitude [m] according to the standart atmosphere
 
-    #elevato to pressure
-    Earth_G = 9.80665# Acceleration due to gravity (m s-2)
-    EarthR = 6.3756766E6 # Average earths radius (m)
+    Parameters:
+    z (float)
+
+    Returns:
+    zp (float)
+    """
+    P0=101325 #air pressure sea level
+    T0=288.15 #standart temperature
+
+    Earth_G = 9.80665 # acceleration due to gravity (m s-2)
+    EarthR = 6.3756766E6 # average earths radius (m)
     Md = 28.966 # Molecular weight of dry air
     R_star = 8.3145 # Universal gas constant J/molK
     stlapse = -0.0065 # standard lapse rate K/m
     H1 = (EarthR * z) /(EarthR + z)
     HB = 0.0
     zp = P0*(T0/(T0+stlapse*(H1-HB)))**((Earth_G*Md)/(R_star*stlapse*1000))
-    zp = zp/100.0  
+    zp = zp/100.0
     return(zp)
 
 def SatPresWatVap(Ta):
-    '''
-    I: air temperature [K]
-    O: saturation pressure of water vapour in air [hPa]
-    
-    copied from Corripio, 2013 github:
-    https://github.com/cran/insol/blob/master/R/wvapsat.R
-    
-    based on:
-        Lowe, P. R.: 1977, An approximating polynomial for the computation of saturation vapor pressure,
-        Journal of Applied Meteorology, 16, 100-103.      
-    '''
+    """
+    Computes saturation pressure of water vapour in air [hPa] for  given air temperature [K]
+    Lowe (1977)
+
+    Parameters:
+    Ta (float)
+
+    Returns:
+    watervaporpressure (float)
+    """
 
     tempcl = Ta
-    a0     = 6984.505294	
+    a0     = 6984.505294
     a1     = -188.9039310
-    a2     = 2.133357675	
-    a3     = -1.288580973e-2	
+    a2     = 2.133357675
+    a3     = -1.288580973e-2
     a4     = 4.393587233e-5
     a5     = -8.023923082e-8
     a6     = 6.136820929e-11
-    
-    return( a0+tempcl*(a1+tempcl*(a2+tempcl*(a3+tempcl*(a4+tempcl*(a5+tempcl*a6))))) )    
+
+    watervaporpressure = a0+tempcl*(a1+tempcl*(a2+tempcl*(a3+tempcl*(a4+tempcl*(a5+tempcl*a6)))))
+
+    return watervaporpressure
 
 #ENERGY FLUXES AND DEBRIS THICKNESS
 
 def insolation(zenith,jd,height,RH,tempK):
-    
-    '''
-    based on:
-    Iqbal, M. (1983) An Introduction to Solar Radiation
-    Bird, R. E. and Hulstrom, R. L. (1981b) A simplified clear sky model for direct and diffuse insolation on horizontal surfaces
-    '''
+    """
+    Computes direct and diffusive component of incoming shortwave radiation (Wm-2)
+    without topographic effects
+
+    Solar geometry is based von Iqbal (1983)
+    Atmospheric transmissivity is based von Bird & Hulstrom (1981b)
+
+    Parameters:
+        zenith (float)
+        jd (float)
+        height (float)
+        RH (float)
+        tempK (float)
+
+    Returns:
+        radiation (tuple)
+            In (float)
+            Id (float)
+    """
+
     O3 = 0.3
     visibility = 30
-    alphag = 0.2 
-    
-    print('zenith:' , zenith)
+    alphag = 0.2
+
     Isc = 1361.8   # solar constant (Wm^(-2)) (1)
     theta = np.radians(zenith)
     ssctalb = 0.9  # single scattering albedo (aerosols)(Iqbal, 1983)
@@ -343,10 +442,10 @@ def insolation(zenith,jd,height,RH,tempK):
     TauG = np.exp(-0.0127*(Ma**0.26))
     TauW = 1.0-2.4959*(Wprec*Mr)*( (1.0+79.034*(Wprec*Mr))**0.6828 + 6.385*(Wprec*Mr) )**(-1)
     TauA = ( 0.97-1.265*(visibility**(-0.66)) )**(Ma**0.9)   #Machler, 1983
-    TauTotal = TauR*TauO*TauG*TauW*TauA     
-    
+    TauTotal = TauR*TauO*TauG*TauW*TauA
+
     In = 0.9751*rho2*Isc*TauTotal
-    
+
     tauaa = 1.0-(1.0-ssctalb)*(1.0-Ma+Ma**1.06)*(1.0-TauA)
     Idr = 0.79*rho2*Isc*np.cos(theta)*TauO*TauG*TauW*tauaa*0.5*(1.0-TauR)/(1.0-Ma+Ma**(1.02))
     tauas = (TauA)/tauaa
@@ -354,145 +453,184 @@ def insolation(zenith,jd,height,RH,tempK):
     alpha_atmos = 0.0685+(1.0-Fc)*(1.0-tauas)
     Idm = (In*np.cos(theta)+Idr+Ida)*alphag*alpha_atmos/(1.0-alphag*alpha_atmos)
     Id = Idr+Ida+Idm
-   
+    radiation = (In, Id)
 
-    return (In, Id)
+    return radiation
 
 def calc_sin(dt_object,latitude,longitude,timezone, dem, res, relhum, TairK, shadow_surrounding=None):
-    
-    '''
-    zenith, jd, elevation, vis, relhum, TairK, O3, albedo_surrounding, shadow_surrounding (cast shadow)
-    '''
-    jd = JD(dt_object)
-    
+    """
+    Computes incident shortwave radiation for each pixel. Illumination intensity (hillshade)
+    base on the DEM (or dem of surrounding terrain) determines how much radiation is received by
+    each pixel.
 
+    Parameters:
+        dt_object (datetime object)
+        latitude (float)
+        longitude (float)
+        timezone (int)
+        dem (numpy 2darray)
+        res (float)
+        relhum (float)
+        TairK (float)
+        shadow_surrounding (None or 2darray)
+
+    Returns:
+        Iglobal (2darray)
+
+    """
+
+    jd = JD(dt_object)
     sv = sunvector(jd,latitude,longitude,timezone)
     zenith = sunpos(sv)[1]
-    
-    
-    mean_elevation = np.nanmean(dem) #np.mean(dem[dem>0])
-    #res = dem.geotransform[1] # richdem required
-    
+    mean_elevation = np.nanmean(dem)
     cgr = gradient(dem, res, length_y=None)
-    
-    #Computes the intensity of illumination over a surface (DEM), according to the sun
 
     hsh = hill_shade(cgr,sv)
-    #https://meteoexploration.com/blog/index.php/2022/01/09/hands-on-solar-radiation/
     hsh = (hsh+np.absolute(hsh))/2
     sh = project_shadows(dem, np.array(sv), res, dy=None)
     shade = hsh*sh
-    
+
+    #add shade from surrounding terrain
     if shadow_surrounding is not None:
-        shade= shade*shadow_surrounding
-    
+        shade=shade*shadow_surrounding
+
     insol = insolation(zenith, jd, mean_elevation, relhum, TairK)
-    
+
     if zenith<90:
         direct = insol[0]
         diffuse = insol[1]
     else:
-        direct = 0 
+        direct = 0
         diffuse = 0
-    
+
     print('Id: ', direct)
     print('In: ', diffuse)
-    
+
     Iglobal = np.zeros(dem.shape)
     Iglobal = Iglobal + ( direct * shade + diffuse)
-    
+
     return  Iglobal
 
-def calc_snet(albedo, insolation):
-    snet = (1-albedo)*insolation
+def calc_snet(albedo, swin):
+    """
+    Computes net shortwave radiation (Wm-2) from albedo and incoming shortwave radiation (Wm-2)
+
+    Parameters:
+    albedo (float or 2darray)
+    swin (float or 2darray)
+
+    Returns:
+    lnet (loat or 2darray)
+    """
+
+    snet = (1-albedo)*swin
+
     return snet
 
 def calc_lout(emissivity, lst):
-    
+    """
+    Computes outgoinf longwave radiation (Wm-2) based on emssivity and land surface temperature (K)
+
+    Parameters:
+    emissivity (float or 2darray)
+    lst (float or 2darray)
+
+    Returns:
+    lout (loat or 2darray)
+    """
+    # convert C to K
     if np.nanmean(lst)<200:
-        
         lst = lst+273.15
-    
-    stefanbolzman_cont=5.67e-08
-  
-    # emissivity correction was applied while trad to tkin conversion
-    lout = stefanbolzman_cont * emissivity * (lst**4)
+
+    Sboltzman_const=5.67e-08
+
+    lout = Sboltzman_const * emissivity * (lst**4) # Stefan Boltzmann Law
     return lout
-    
+
 def calc_lnet(lin, lout):
-    return lin-lout  
+    """
+    Computes net longwave radiation (Wm-2)
+
+    Parameters:
+    lin (float or 2darray)
+    lout (loat or 2darray)
+
+    Returns:
+    lnet (float or 2darray)
+    """
+    lnet = lin-lout
+    return lnet
 
 def calc_shf(TairK, lst, elevation, windspeed, zt, zu, roughness):
+    """
+    Computes sensible heat flux (Wm-2) based on land surface temperature (K), air temperature (K)
+    wind speed in (ms-1) and surface roughness length (m), Nicholson and Benn (2006)
 
-    '''
-    TairK [K]: air temperature
-    lst [K]: land surface temperature
-    elevation [m]: mean DEM elevation
-    windspeed [m/s]: from ERA5
-    zt [m]: air temperature measurement height  (2m for ERA5)
-    zu [m]: wind speed measurement height (10m for ERA5)
-    roughness [-]: surface roughness length  (0.016)
-    
-    
-    '''
-    karmans_const = 0.41
-    c_air = 1010 # specific heat capacity air
-    
-    P0 = 101325 # air pressure at sealevel in Pa
-    rho_sl = 1.29 # air density sealeavel [kgm–3] Nicholson and Benn, 2006 
-    
-    P = z2p(elevation)*100 # air pressure at mean DEM elevation in Pa 
-    #print('pressure local:' , P0)   
-    
-    rho_local = rho_sl *  (P/P0) #[kgm–3] air density local
-    #print('air density local: ', rho_local)
-    
+    Parameters:
+        TairK (float or 2darray)
+        lst (float or 2darray)
+        elevation (float)
+        windspeed (float)
+        zt (int)
+        zu (int)
+        roughness (float)
+
+    Returns:
+        H (float or 2darray)
+    """
+
+    karmans_const = 0.41 # Kármáns constant
+    c_air = 1010 # specific heat capacity air (J kg-1 K-1)
+    P0 = 101325 # air pressure at sealevel in (Pa)
+    rho_sl = 1.29 # air density sealeavel (kgm–3)
+    P = z2p(elevation)*100 # air pressure (Pa)
+    rho_local = rho_sl *  (P/P0) # air density local (kgm-3)
+
     A = (karmans_const**2) / (np.log(zu / roughness)*np.log(zt / roughness)) # dimensionless bulk transfer coefficient
 
-    H = rho_local * c_air * windspeed * (TairK - lst) * A  
-    #print('mean H:', np.nanmean(H))
+    H = rho_local * c_air * windspeed * (TairK - lst) * A
     return H
-    
+
 def quadraticEq(a, b, c):
-    
+    """
+    Quadratic formula, provides solution for quadratic equation
+    """
     D = b**2 - (4*a*c)
-    
     x0 = (-b + np.sqrt(D)) / (2*a)
     x1 = (-b - np.sqrt(D)) / (2*a)
-    
+
     return x0, x1
-    
+
 def debristhickness_prediction(sw, lw, H, Ts, Ta, w_rate, params):
     '''
-    Function to estimate debris thickness by solving quadratic equation. If warming rate is close to 
-    threshold, debris thickness is estimated using a linear model
-    
-    Ts and Ta in °C
+    Solves the surface energy balance for debris thickness. Net shortwave (Wm-2),
+    net longwave (Wm-2), sensible heat flux (Wm-2), land surface temperature (K),
+    warming rate (Ks-1) and air temperature (K) need to be determinded in advance.
+    Additional parameters are stored in a dictionary.
+
+    Assumes linear temperature gradient with the layer of debris.
+
+    Parameters:
+        sw (ndarray)
+        lw (ndarray)
+        H (ndarray)
+        Ts (ndarray)
+        Ta (ndarray)
+        w_rate (ndarray)
+        params (dictionary)
+
+    Returns:
+        x0 (ndarray)
     '''
-    
-    
+
     density_debris = params['density_d']
     heat_capacity_debris = params['c_d']
     thermal_conductivity = -params['k_eff']
-    
+
     a = (density_debris * heat_capacity_debris * w_rate) * -1
     b = sw + lw + H
     c = thermal_conductivity * Ts
-    
+
     x0, x1 = quadraticEq(a, b, c)
-    
-    return x0 #, (a, b, c)
-    
-def hillshade(array,azimuth,angle_altitude):
-    azimuth = 360.0 - azimuth 
-    
-    x, y = np.gradient(array)
-    slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
-    aspect = np.arctan2(-x, y)
-    azimuthrad = azimuth*np.pi/180.
-    altituderad = angle_altitude*np.pi/180.
- 
-    shaded = np.sin(altituderad)*np.sin(slope) + np.cos(altituderad)*np.cos(slope)*np.cos((azimuthrad - np.pi/2.) - aspect)
-    
-    return 255*(shaded + 1)/2
-    
+
+    return x0
